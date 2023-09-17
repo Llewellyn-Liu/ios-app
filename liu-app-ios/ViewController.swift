6f7a6d2f4053398dace5db997746f99b278bb4af
runlin liu <liurunlin0213@live.com>
2023-09-17 16:04:12 -0700

    Initial Commit

---
+++ liu-app-ios/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  liu-app-ios
+//
+//  Created by Runlin Liu on 9/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ liu-app-iosUITests/liu_app_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  liu_app_iosUITestsLaunchTests.swift
+//  liu-app-iosUITests
+//
+//  Created by Runlin Liu on 9/17/23.
+//
+
+import XCTest
+
+final class liu_app_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
