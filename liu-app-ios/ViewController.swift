b7c393eb17f93e4fea37ce055938ba00ce03e2a8
runlin liu <liurunlin0213@live.com>
2023-09-17 20:33:42 -0700

    Assignment 1

--- liu-app-ios/ViewController.swift
+++ liu-app-ios/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var consoleLabel: UILabel!
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("Loaded.")
     }
 
-
+    @IBAction func HiBtn(_ sender: Any) {
+        consoleLabel.text = "Hello User!"
+    }
+    
+    @IBAction func seattleBtn(_ sender: Any) {
+        imageView.image = UIImage(named: "Seattle-View")
+    }
+    
+    @IBAction func austinBtn(_ sender: Any) {
+        imageView.image = UIImage(named: "Austin-View")
+    }
+    
+    @IBAction func laBtn(_ sender: Any) {
+        imageView.image = UIImage(named: "LA-View")
+    }
+    
+    @IBAction func nyBtn(_ sender: Any) {
+        imageView.image = UIImage(named: "NYC-View")
+    }
+    
+    @IBAction func vegasBtn(_ sender: Any) {
+        imageView.image = UIImage(named: "Vegas-View")
+    }
+    
 }
 
